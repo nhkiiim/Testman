cdd12b7753526ec2c068b161fecbe3c5f5d7a0cb
edenist-x-x <yaron_e5@naver.com>
2021-11-13 15:32:18 +0900

    add : modified contents parameters

--- frontend/components/Content.jsx
+++ frontend/components/Content.jsx
@@ -7,7 +7,7 @@ import TabBar from "./Tabs/TabBar";
 import { useDispatch, useSelector } from "react-redux";
 import * as tabActions from "../store/modules/tab";
 
-const Content = ({ data, no }) => {
+const Content = ({ current }) => {
   // const [tabs, setTabs] = useState([
   //   {
   //     index: Math.random(0, 10) * 10,
@@ -17,15 +17,15 @@ const Content = ({ data, no }) => {
   //     response: "test",
   //   },
   // ]);
-  const tabs = useSelector((state) => state.tab.tabs)
+  const tabs = useSelector((state) => state.tab.tabs);
   const [showModal, setShowModal] = useState(false);
   const [requestTabs] = useState(["Params", "Authorization", "Headers", "Body", "Settings"]);
-  const tabIndex = useSelector((state) => state.tab.tabIndex)
+  const tabIndex = useSelector((state) => state.tab.tabIndex);
   const [requestTabIndex, setRequestTabIndex] = useState(0);
-  const [selectItem, setSelectItem] = useState('')
-  const dispatch = useDispatch()
+  const [selectItem, setSelectItem] = useState("");
+  const dispatch = useDispatch();
   const handleTabChange = (index) => {
-    console.log('handletabchange', index)
+    console.log("handletabchange", index);
     dispatch(tabActions.setTabIndexState(index));
   };
   const handleRequestTabChange = (index) => {
@@ -42,8 +42,8 @@ const Content = ({ data, no }) => {
       type: "GET",
       URL: "",
       response: {},
-    }
-    dispatch(tabActions.setTabs(payload))
+    };
+    dispatch(tabActions.setTabs(payload));
     dispatch(tabActions.setTabIndexState(tabs.length));
   };
 
@@ -52,42 +52,42 @@ const Content = ({ data, no }) => {
       tab.index !== selectTab.index;
     });
     // console.log(tempTabs)
-    dispatch(tabActions.setTabs(tempTabs))
+    dispatch(tabActions.setTabs(tempTabs));
   };
-  const [parsingParams, setParsingPrams] = useState({})
+  const [parsingParams, setParsingPrams] = useState({});
   const request = useSelector((state) => state.api.request);
   const token = useSelector((state) => state.user.token);
-  const [parsingHeaders, setParsingHeaders] = useState({})
+  const [parsingHeaders, setParsingHeaders] = useState({});
   const handleSubmit = () => {
-    const paramsJson = request.params
+    const paramsJson = request.params;
     // console.log(paramsJson.constructor)
     // console.log(Object.keys(paramsJson).length)
     // console.log(paramsJson)
-    if (paramsJson.constructor === Array){
-      paramsJson.forEach(array => {
-        const copied = parsingParams
-        copied[array.paramKey] = array.paramValue
-        setParsingPrams(copied)
+    if (paramsJson.constructor === Array) {
+      paramsJson.forEach((array) => {
+        const copied = parsingParams;
+        copied[array.paramKey] = array.paramValue;
+        setParsingPrams(copied);
       });
     }
 
-    const headersJson = request.headers
-    if (headersJson.constructor === Array){
-      headersJson.forEach(array => {
-        const copied = parsingHeaders
-        copied[array.paramKey] = array.paramValue
-        setParsingHeaders(copied)
-      })
+    const headersJson = request.headers;
+    if (headersJson.constructor === Array) {
+      headersJson.forEach((array) => {
+        const copied = parsingHeaders;
+        copied[array.paramKey] = array.paramValue;
+        setParsingHeaders(copied);
+      });
     }
     const payload = {
-      "address": "http://www.testsman.com:8080",
-      "httpMethod": request.payload.httpMethod,
-      "headers": parsingHeaders,
-      "params": parsingParams,
-      "path": request.uri,
-      "tabSeq": tabIndex,
-      "workspaceSeq": 0
-    }
+      address: "http://www.testsman.com:8080",
+      httpMethod: request.payload.httpMethod,
+      headers: parsingHeaders,
+      params: parsingParams,
+      path: request.uri,
+      tabSeq: tabIndex,
+      workspaceSeq: 0,
+    };
     // const payload2 = {
     //   "workspaceSeq":4,
     //   "tabSeq":1,
@@ -99,7 +99,7 @@ const Content = ({ data, no }) => {
     //       "title": "new project",
     //       "url": "www.ssafy.com"
     //   }
-  // }
+    // }
     axios({
       method: "POST",
       url: "/api/api-result",
@@ -109,11 +109,11 @@ const Content = ({ data, no }) => {
       data: payload,
     })
       .then((res) => {
-        console.log(res.data.response)
+        console.log(res.data.response);
       })
       .catch((error) => {
-        console.log(error)
-      })
+        console.log(error);
+      });
   };
 
   const handleURLChange = (e) => {
@@ -122,68 +122,68 @@ const Content = ({ data, no }) => {
     dispatch(tabActions.setTabs(tempTabs));
   };
 
-  const [collectionList, setCollectionList] = useState([])
+  const [collectionList, setCollectionList] = useState([]);
 
   const getCollectionList = () => {
     axios({
       method: "GET",
-      url: `/api/collections/${no}`,
+      url: `/api/collections/${current.seq}`,
       headers: {
         Authorization: `Bearer ${token}`,
-      }
+      },
     })
       .then((res) => {
-        console.log(res.data.response.collectionList)
-        setCollectionList(res.data.response.collectionList)
+        console.log(res.data.response.collectionList);
+        setCollectionList(res.data.response.collectionList);
       })
       .catch((error) => {
-        console.log(error)
-      })
-  }
+        console.log(error);
+      });
+  };
 
   const clickSaveBtn = () => {
-    setShowModal(true)
-    getCollectionList()
-  }
+    setShowModal(true);
+    getCollectionList();
+  };
   const [btnDescription, setBtnDescription] = useState(false);
 
-  const [newBtn, setNewBtn] = useState(false)
-  const [newCollection, setNewCollection] = useState('')
+  const [newBtn, setNewBtn] = useState(false);
+  const [newCollection, setNewCollection] = useState("");
 
   const handleNewCollection = (e) => {
-    setNewCollection(e.target.value)
-  }
+    setNewCollection(e.target.value);
+  };
   const clickSaveCollections = () => {
-    const paramsJson = request.params
+    const paramsJson = request.params;
     // console.log(paramsJson.constructor)
     // console.log(Object.keys(paramsJson).length)
     // console.log(paramsJson)
-    if (paramsJson.constructor === Array){
-      paramsJson.forEach(array => {
-        const copied = parsingParams
-        copied[array.paramKey] = array.paramValue
-        setParsingPrams(copied)
+    if (paramsJson.constructor === Array) {
+      paramsJson.forEach((array) => {
+        const copied = parsingParams;
+        copied[array.paramKey] = array.paramValue;
+        setParsingPrams(copied);
       });
     }
 
-    const headersJson = request.headers
-    if (headersJson.constructor === Array){
-      headersJson.forEach(array => {
-        const copied = parsingHeaders
-        copied[array.paramKey] = array.paramValue
-        setParsingHeaders(copied)
-      })
+    const headersJson = request.headers;
+    if (headersJson.constructor === Array) {
+      headersJson.forEach((array) => {
+        const copied = parsingHeaders;
+        copied[array.paramKey] = array.paramValue;
+        setParsingHeaders(copied);
+      });
     }
     const payload = {
-      "address": "http://www.testsman.com:8080",
-      "collectionSeq": selectItem.seq,
-      "headers": parsingHeaders,
-      "httpMethod": request.payload.httpMethod,
-      "params": parsingParams,
-      "path": request.uri,
-      "seq": 15,
-    }
-    console.log('payload', payload)
+      address: "http://www.testsman.com:8080",
+      collectionSeq: selectItem.seq,
+      headers: parsingHeaders,
+      httpMethod: request.payload.httpMethod,
+      params: parsingParams,
+      path: request.uri,
+      seq: 15,
+    };
+    console.log("payload", payload);
     axios({
       method: "PATCH",
       url: "/api/tabs",
@@ -192,148 +192,154 @@ const Content = ({ data, no }) => {
       },
       data: payload,
     })
-    .then((res) => {
-      console.log('success', res.data.response)
-      setShowModal(false)
-    })
-    .catch((error) => {
-      console.log(error)
-      setShowModal(false)
-    })
-  }
+      .then((res) => {
+        console.log("success", res.data.response);
+        setShowModal(false);
+      })
+      .catch((error) => {
+        console.log(error);
+        setShowModal(false);
+      });
+  };
 
   const handleCollectionEnter = (e) => {
-    if (e.key==="Enter") {
+    if (e.key === "Enter") {
       axios({
-        method:"POST",
+        method: "POST",
         url: "/api/collections",
         headers: {
           Authorization: `Bearer ${token}`,
         },
         data: {
-          "name":newCollection,
-          "workspaceSeq": no
-        }
+          name: newCollection,
+          workspaceSeq: current.seq,
+        },
       })
         .then((res) => {
-          getCollectionList()
-          setNewCollection('')
+          getCollectionList();
+          setNewCollection("");
         })
         .catch((error) => {
-          console.log(error)
-        })
+          console.log(error);
+        });
     }
-  }
+  };
 
   const clickItem = (item) => () => {
-    console.log(item)
-    setSelectItem(item)
-  }
-
+    console.log(item);
+    setSelectItem(item);
+  };
 
   useEffect(async () => {
     const initTab = {
       index: Math.random(0, 10) * 10,
-      name: data.title,
+      name: current.title,
       type: "GET",
-      URL: data.url,
+      URL: current.url,
       response: {},
-    }
-    dispatch(tabActions.setTabs(initTab))
+    };
+    dispatch(tabActions.setTabs(initTab));
     dispatch(tabActions.setTabIndexState(tabs.length));
   }, []);
   return (
     <div>
-      {showModal?
-          <>
-              (<div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
-              <div className="relative w-auto my-6 mx-auto max-w-3xl">
-                {/*content*/}
-                <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-                  {/*header*/}
-                  <div className="flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t">
-                    <h3 className="text-3xl font-semibold">Save request</h3>
-                  </div>
-                  {/*body*/}
-                  <div className="relative p-6 flex-auto">
-                    <form className="flex flex-col w-[350px]" method="POST" action="#">
-      
-                      <div className="mb-6 pt-3 rounded">
-                        <label className="block text-sm font-bold mb-0 ml-3" for="URL">
-                          Save to {selectItem.name}
-                        </label>
-                        {collectionList.map((item, i) => (
-                          <div key={i} onClick={clickItem(item)}>
-                            {item.name}
-                          </div>
-                        ))}
-                        {!newBtn ?
-                          <div>
-                            <button className="bg-white w-full mb-0 ml-3 text-left hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-gray-200 focus:ring-opacity-50" onClick={() => {
-                              setNewBtn(true)
-                            }}>
-                              New Collection
-                            </button>
-                          </div>
-                          :
-                          <div>
-                            <input type="text" placeholder="Enter collection name" onKeyPress={handleCollectionEnter} onChange={handleNewCollection} value={newCollection}/>
-                          </div>
-                        }
-                      </div>
-                    </form>
-                  </div>
-                  {/*footer*/}
-                  <div className="flex items-center justify-between p-6 border-t border-solid border-blueGray-200 rounded-b">
-                    <button
-                      className="text-red-500 background-transparent font-bold px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                      type="button"
-                      onClick={() => setShowModal(false)}
-                    >
-                      Cancel
-                    </button>
-                    <button
-                      className="bg-purple-500 text-white active:bg-emerald-600 font-bold text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                      type="button"
-                      onClick={clickSaveCollections}
-                    >
-                      Save
-                    </button>
-                  </div>
+      {showModal ? (
+        <>
+          (
+          <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+            <div className="relative w-auto my-6 mx-auto max-w-3xl">
+              {/*content*/}
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                {/*header*/}
+                <div className="flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t">
+                  <h3 className="text-3xl font-semibold">Save request</h3>
+                </div>
+                {/*body*/}
+                <div className="relative p-6 flex-auto">
+                  <form className="flex flex-col w-[350px]" method="POST" action="#">
+                    <div className="mb-6 pt-3 rounded">
+                      <label className="block text-sm font-bold mb-0 ml-3" for="URL">
+                        Save to {selectItem.name}
+                      </label>
+                      {collectionList.map((item, i) => (
+                        <div key={i} onClick={clickItem(item)}>
+                          {item.name}
+                        </div>
+                      ))}
+                      {!newBtn ? (
+                        <div>
+                          <button
+                            className="bg-white w-full mb-0 ml-3 text-left hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-gray-200 focus:ring-opacity-50"
+                            onClick={() => {
+                              setNewBtn(true);
+                            }}
+                          >
+                            New Collection
+                          </button>
+                        </div>
+                      ) : (
+                        <div>
+                          <input
+                            type="text"
+                            placeholder="Enter collection name"
+                            onKeyPress={handleCollectionEnter}
+                            onChange={handleNewCollection}
+                            value={newCollection}
+                          />
+                        </div>
+                      )}
+                    </div>
+                  </form>
+                </div>
+                {/*footer*/}
+                <div className="flex items-center justify-between p-6 border-t border-solid border-blueGray-200 rounded-b">
+                  <button
+                    className="text-red-500 background-transparent font-bold px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={() => setShowModal(false)}
+                  >
+                    Cancel
+                  </button>
+                  <button
+                    className="bg-purple-500 text-white active:bg-emerald-600 font-bold text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={clickSaveCollections}
+                  >
+                    Save
+                  </button>
                 </div>
               </div>
             </div>
-            <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>)
-            </>
-          :
-          null
-          }
-    <div className="ml-[312px]">
-      <p className="mt-5 font-bold text-xl">
-        {data.title} ({data.url})
-      </p>
-      <p className="mt-2 text-gray-400 text-xs">{data.description}</p>
-      <TabBar
-        tabs={tabs}
-        tabIndex={tabIndex}
-        handleTabChange={handleTabChange}
-        handleNewTab={handleNewTab}
-        handleRemoveTab={handleRemoveTab}
-      />
-      <RequestInput
-        tabs={tabs[tabIndex]}
-        handleURLChange={handleURLChange}
-        handleSubmit={handleSubmit}
-        url={data.url}
-        clickSaveBtn={clickSaveBtn}
-      />
-      <RequestOptionsSector
-        requestTabs={requestTabs}
-        tabIndex={requestTabIndex}
-        handleRequestTabChange={(index) => handleRequestTabChange(index)}
-      />
-      <RequestOptions requestTabIndex={requestTabIndex}/>
-    </div>
+          </div>
+          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>)
+        </>
+      ) : null}
+      <div className="ml-[312px]">
+        <p className="mt-5 font-bold text-xl">
+          {current.title} ({current.url})
+        </p>
+        <p className="mt-2 text-gray-400 text-xs">{current.description}</p>
+        <TabBar
+          tabs={tabs}
+          tabIndex={tabIndex}
+          handleTabChange={handleTabChange}
+          handleNewTab={handleNewTab}
+          handleRemoveTab={handleRemoveTab}
+        />
+        <RequestInput
+          tabs={tabs[tabIndex]}
+          handleURLChange={handleURLChange}
+          handleSubmit={handleSubmit}
+          url={current.url}
+          clickSaveBtn={clickSaveBtn}
+        />
+        <RequestOptionsSector
+          requestTabs={requestTabs}
+          tabIndex={requestTabIndex}
+          handleRequestTabChange={(index) => handleRequestTabChange(index)}
+        />
+        <RequestOptions requestTabIndex={requestTabIndex} />
+      </div>
     </div>
   );
 };
